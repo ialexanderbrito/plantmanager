0200614264d222f21035e7ffbfc8cc084d923558
Alexander <xanderzinho26@gmail.com>
2021-04-24 23:41:07 -0300

    :sparkles: Atualizado confirmação

--- src/pages/UserIdentification/index.tsx
+++ src/pages/UserIdentification/index.tsx
@@ -49,9 +49,20 @@ export function UserIdentification() {
     if (!name)
       return Alert.alert('Me diz como chamar você 😢', 'Por favorzinho.');
 
-    await AsyncStorage.setItem('@plantmanager:user', name);
+    try {
+      await AsyncStorage.setItem('@plantmanager:user', name);
 
-    navigation.navigate('Confirmation');
+      navigation.navigate('Confirmation', {
+        title: 'Prontinho',
+        subtitle:
+          'Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+        buttonTitle: 'Começar',
+        icon: 'smile',
+        nextScreen: 'PlantSelect',
+      });
+    } catch {
+      Alert.alert('Não foi possível salvar o seu nome. 😢');
+    }
   }
 
   return (
