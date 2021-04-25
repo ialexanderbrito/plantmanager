16f1c789cdb225f212bb73fd7edf1bdcbfe637cc
Alexander <xanderzinho26@gmail.com>
2021-04-25 15:28:39 -0300

    :rocket: Deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.29:3333',
+  baseURL: 'https://my-json-server.typicode.com/ialexanderbrito/plantmanager',
 });
 
 export default api;
